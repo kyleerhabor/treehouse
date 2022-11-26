88c397271d74d1c7d74c89772695c2e876974352
Kyle Erhabor <kyleerhabor@gmail.com>
2022-11-26 17:20:08 -0500

    Add comments on serving files compiled by shadow

--- src/main/kyleerhabor/treehouse/server.clj
+++ src/main/kyleerhabor/treehouse/server.clj
@@ -79,6 +79,17 @@
 
 (def handler (rr/ring-handler router
                (rr/routes
+                 ;; Shadow produces a main.js file which is meant to be consumed by the user when loading the page. In
+                 ;; development, however, it also produces a cljs-runtime folder, which is not required for release mode.
+                 ;; As such, in production, users should not request resources from the cljs-runtime folder. The server,
+                 ;; however, cannot tell whether or not it is running in development or release mode. For that reason,
+                 ;; the server should make no attempt to filter which files from the resources/public folder should be
+                 ;; presented. Instead, the folder should be allowed to be populated by shadow in development mode, but,
+                 ;; when running on, say, a dedicated server, the cljs-runtime folder should be excluded from the file
+                 ;; system.
+                 ;;
+                 ;; Another solution may be to purge the cljs-runtime folder when compiling under release mode, though
+                 ;; this would be slightly more complicated and annoying when switching back to development mode.
                  (rr/create-resource-handler {:path "/"})
                  (rr/create-default-handler (assoc default-handler-options :not-found default-handler)))))
 
